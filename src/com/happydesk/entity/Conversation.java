8539a91dda263ddc0a8cc61de3dd1f9f5829a54e
coolom-happydesk <omprakashyadav@Oms-MacBook-Pro.local>
2015-12-25 16:52:08 +0530

    table name corrected

--- src/com/happydesk/entity/Conversation.java
+++ src/com/happydesk/entity/Conversation.java
@@ -12,7 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "complainer")
+@Table(name = "conversation")
 public class Conversation extends BaseEntity<Integer> {
 	
 
